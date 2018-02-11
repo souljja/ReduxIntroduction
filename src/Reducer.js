fc7c1f38e2dc20d061ae30be624ce4cf5ac8c7a9
souljja <andrejjbogorodickijj@gmail.com>
2018-02-11 15:52:00 +0300

    replace string with object in action field

--- src/Reducer.js
+++ src/Reducer.js
@@ -1,20 +1,23 @@
 let state = 0;
 
 function updateState(state, action) {
-  if (action === "INCREMENT") {
-    return state + 1;
-  } else if (action === "DECREMENT") {
-    return state - 1;
+  if (action.type === "INCREMENT") {
+    return state + action.amount;
+  } else if (action.type === "DECREMENT") {
+    return state - action.amount;
   } else {
     return state;
   }
 }
 
-state = updateState(state, "INCREMENT");
+const incrementAction = { type: "INCREMENT", amount: 5 }
+const decrementAction = { type: "DECREMENT", amount: 3 };
+
+state = updateState(state, incrementAction);
 console.log(state);
 
-state = updateState(state, "DECREMENT");
+state = updateState(state, decrementAction);
 console.log(state);
 
-state = updateState(state, "NOTHING");
+state = updateState(state, {});
 console.log(state);
